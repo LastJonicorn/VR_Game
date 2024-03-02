69ac762e5665407318a554a693f0f22fb5159917
lastjonicorn <joni.k.lampela@gmail.com>
2024-03-02 16:55:23 +0200

    Added a gun WIP

--- Assets/Scripts/ActivateTeleportationRay.cs
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -10,9 +10,11 @@ public class ActivateTeleportationRay : MonoBehaviour
 
     public InputActionProperty rightActivate;
 
+    public InputActionProperty rightCancel;
+
     // Update is called once per frame
     void Update()
     {
-        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1);
+        rightTeleportation.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1);
     }
 }
